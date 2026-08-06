8c91cda8af3f61ba9239ea70543f7df46b630184
keshavsoft <nknnkumar@yahoo.com>
2026-08-06 09:40:43 +0530

    14 sub route fixed

--- test/v11/fromRoutesJs/test.js
+++ test/v11/fromRoutesJs/test.js
@@ -3,12 +3,13 @@ import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-import { runSync } from "../../../index.js";
+import runSync from "../../../index.js";
+const folderName = "v1";
 
 const fromNpm = runSync({
+    raka: folderName, poka: folderName,
     inFileType: "fromRoutesJs",
-    folderName: "v1",
-    toPath: __dirname
+    toPath: __dirname,
 });
 
 console.log("aaaaaa : ", fromNpm);
